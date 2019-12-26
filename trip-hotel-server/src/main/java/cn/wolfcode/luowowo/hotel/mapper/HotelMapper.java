8e7b94ae83187c9835b2a4109071cbdebedb620e
jkunbeloved <1271623149@qq.com>
2019-12-26 20:18:56 +0800

    酒店页面第二次修改

--- trip-hotel-server/src/main/java/cn/wolfcode/luowowo/hotel/mapper/HotelMapper.java
+++ trip-hotel-server/src/main/java/cn/wolfcode/luowowo/hotel/mapper/HotelMapper.java
@@ -1,6 +1,8 @@
 package cn.wolfcode.luowowo.hotel.mapper;
 
 import cn.wolfcode.luowowo.hotel.domain.Hotel;
+import cn.wolfcode.luowowo.hotel.query.HotelQuery;
+
 import java.util.List;
 
 public interface HotelMapper {
@@ -15,4 +17,7 @@ public interface HotelMapper {
     int updateByPrimaryKey(Hotel record);
 
     List<Hotel> selectSpecialOfferHotelByDestIdTop8(Long destId);
+
+    List<Hotel> selectByDestId(HotelQuery qo);
+
 }
